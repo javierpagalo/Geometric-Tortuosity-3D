d3a9cd5460b2e1f82b823e74e49073734dc4ef0f
Joseph Danilo Avila Alvarez <josdavil@espol.edu.ec>
2020-06-19 20:49:20 -0500

    time generation

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from Tortuosity import *
 import porespy as ps
 from PorousMedium import *
 import shutil
+import time
+
+import time
+
+
 
 
 """
@@ -15,15 +20,22 @@ result=findPoints(maze, "E")
 print(maze.shape)"""
 
 """SIZE" is the size that the porous medium will have, NUMBER_MEDIUM the media quantities 
-They will be saved in the 3dmodels folder """"
-SIZE = 10
+They will be saved in the 3dmodels folder """
+SIZE = 6
 SHAPE = list(SIZE for _ in range(3))
 NUMBER_MEDIUM=3
+start=time.time()
 for i in range(NUMBER_MEDIUM):
+    startf=time.time()
     generate, maze = generate_blobs(SHAPE, 0.60, 0.5)
     maze = np.logical_not(np.array(maze, dtype=int))
     maze = np.array(maze, dtype=int)
     structure_processing(generate, "blobs"+str(i))
     result = geometric_tortuosity(maze)
     shutil.move("blobs"+str(i)+".stl", "3dmodels/blobs"+str(i)+".stl")
-    print(result)
+    endf=time.time()
+    tiempof=endf-startf
+    print(str(result) +"------"+str(tiempof))
+end=time.time()
+tiempo=end-start
+print("El tiempo de ejucion de "+str(NUMBER_MEDIUM)+":"+str(tiempo))
