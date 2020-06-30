1debbd827eb96006cf4c044171401fa20b0a513e
Joseph Danilo Avila Alvarez <josdavil@espol.edu.ec>
2020-06-29 20:35:39 -0500

    paths format and show paths

--- visualation.py
+++ visualation.py
@@ -4,15 +4,18 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 
 
+def path_format(paths):
+    pathFormat=[]
+    for path in paths:
+        pointers=np.array(path)
+        pathFormat.append((pointers[:,0],pointers[:,1],pointers[:,2]))
+    return pathFormat
+    
+        
 def show_paths_on_porous_medium(paths):
     fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
     for path in paths:
-        print("f")
-    datasets = [{"x":[1,2,3], "y":[1,4,9], "z":[0,0,0], "colour": "red"} for _ in range(6)]
-    print(datasets)
-    for dataset in datasets:
-        ax.plot(dataset["x"], dataset["y"], dataset["z"], color=dataset["colour"])
-
+        ax.plot(path[0], path[1],path[2], color="red")
     plt.show()
 
-show_paths_on_porous_medium("D")
+    
