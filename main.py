55d1eb4f759f233e92d0b03f07a41695751845cd
javierpagalo <javierpagalo@gmail.com>
2020-07-09 18:12:56 -0500

    JustAFile

--- main.py
+++ main.py
@@ -27,9 +27,11 @@ SHAPE = list(SIZE for _ in range(3))
 NUMBER_MEDIUM=5
 start=time.time()
 tortuosity_test=[]
+document=open("Tortu_porsity.txt","a")
+porosity=0.60
 for i in range(NUMBER_MEDIUM):
     startf=time.time()
-    generate, maze = generate_blobs(SHAPE, 0.60, 0.5)
+    generate, maze = generate_blobs(SHAPE, porosity, 0.5)
     maze = np.logical_not(np.array(maze, dtype=int))
     maze = np.array(maze, dtype=int)
     #structure_processing(generate, "blobs"+str(i))
@@ -47,4 +49,11 @@ end=time.time()
 tiempo=end-start
 print("El tiempo de ejecucion de "+str(NUMBER_MEDIUM)+":"+str(tiempo))
 print(tortuosity_test)
+document.write("Porosity:\n"+str(porosity)+"\n")
+document.write("Shape:\n"+str(SIZE)+"\n")
+document.write("Tortuosity:\n")
+for elem in tortuosity_test:
+    document.write(str(elem)+"\n")
+
+document.close()
 
